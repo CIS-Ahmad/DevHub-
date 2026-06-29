366c9b8ee2c5a0d7f171b94fb42b75adde001be2
ICE <152382318+CIS-Ahmad@users.noreply.github.com>
2026-06-29 13:50:22 +0300

    first version of Devhup

---
+++ data.js
@@ -0,0 +1,59 @@
+export const Data = [
+  {
+    ProjectName: "portfolio",
+    ProjectUrl: "https://cis-ahmad.github.io/portfolio/",
+    Note: "html - css only",
+    Type: "css",
+  },
+  {
+    ProjectName: "Solar System",
+    ProjectUrl: "https://cis-ahmad.github.io/Solar-System/",
+    Note: "calculate weight of an object on other planet",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "World countries v1",
+    ProjectUrl:
+      "https://cis-ahmad.github.io/World-countries-data-visualization/",
+    Note: "Statistics Data Only",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "World countries v2",
+    ProjectUrl:
+      "https://cis-ahmad.github.io/World-countries-data-visualization-2/",
+    Note: "Apply filter on country name with types of search tool",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "animation-project v1",
+    ProjectUrl: "https://cis-ahmad.github.io/animation-project-/",
+    Note: "practice on animation",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "leader board",
+    ProjectUrl: "https://cis-ahmad.github.io/leaderboard/",
+    Note: "Local stogre system",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "animation-project v2",
+    ProjectUrl: "https://cis-ahmad.github.io/Animating-Characters/",
+    Note: "practice on animation",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "World countries v3",
+    ProjectUrl:
+      "https://cis-ahmad.github.io/World-countries-data-visualization-3/",
+    Note: "make full World countries Statistics and filter search on country Name, Capital city and on language",
+    Type: "javascript",
+  },
+  {
+    ProjectName: "form validation",
+    ProjectUrl: "https://cis-ahmad.github.io/form-Validate/",
+    Note: "practice on REGEX",
+    Type: "javascript",
+  },
+];
---
+++ script.js
@@ -0,0 +1,66 @@
+import { Data } from "./data.js";
+
+const PROJECTS = [...Data];
+
+function getElementUsingID(ID) {
+  return document.getElementById(ID);
+}
+
+function createElement(
+  name,
+  className,
+  id = null,
+  textContent = "",
+  father = null,
+) {
+  const element = document.createElement(name);
+  if (className) element.classList.add(className);
+
+  if (textContent !== "") {
+    element.textContent = textContent;
+  }
+
+  if (id) {
+    element.id = id;
+  }
+
+  if (father) {
+    const parent = getElementUsingID(father);
+    parent.appendChild(element);
+  } else {
+    document.body.appendChild(element);
+  }
+
+  return element;
+}
+
+function createSectionNotExisting(secID) {
+  createElement("h2", "title", "", secID, "main");
+  createElement("section", "", secID, "", "main");
+}
+
+function createProjectButton(projectName, projectURL, Note, secType, ID) {
+  if (!getElementUsingID(secType)) {
+    createSectionNotExisting(secType);
+  }
+
+  const link = createElement("a", "", `${secType}-${ID}`, "", secType);
+  link.href = projectURL;
+  link.target = "_blank";
+
+  createElement("button", "", "", projectName, `${secType}-${ID}`).title = Note;
+}
+
+function createProjects() {
+  for (let index = 0; index < PROJECTS.length; index++) {
+    createProjectButton(
+      PROJECTS[index].ProjectName,
+      PROJECTS[index].ProjectUrl,
+      PROJECTS[index].Note,
+      PROJECTS[index].Type,
+      index,
+    );
+  }
+}
+
+createProjects();
